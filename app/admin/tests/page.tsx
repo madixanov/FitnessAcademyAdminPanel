06a15029573861b87ffe881f5f7481695d57d45e
madixanov <yoqub.madixanov@gmail.com>
2026-09-25 21:48:19 +0500

    мелкие правки

--- app/admin/tests/page.tsx
+++ app/admin/tests/page.tsx
@@ -366,7 +366,7 @@ export default function AdminTests() {
               >
                 <option value="DRAFT">Черновик</option>
                 <option value="ACTIVE">Активен</option>
-                <option value="FINISHED">Неактивен</option>
+                <option value="FINISHED">Завершен</option>
               </select>
             </div>
 
